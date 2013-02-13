efe7986869d361da8889726a76243d47f02d1bd2
Michael J Salera <mikesalera@mac.com>
2013-02-13 00:24:02 -0800

    Added a date-of-birth field to User and forward engineered after testing migrations

--- grails-app/domain/my/api/User.groovy
+++ grails-app/domain/my/api/User.groovy
@@ -5,11 +5,13 @@ class User {
       String middleInitial
       String lastName
       String email
+	  Date dateOfBirth
 
       static constraints = {
       	     firstName(nullable: false, blank: false)
 	     middleInitial(nullable: true, blank: false)
 	     lastName(nullable: false, blank: false)
 	     email(nullable: false, blank: false)
+		 dateOfBirth(nullable: false)
       }
 }
